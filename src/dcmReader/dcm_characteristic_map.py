427e1e7b0c1b6b3942a2975223ccfccfdccdefe0
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-01-02 17:11:36 +0100

    Update dcm_characteristic_map.py

--- src/dcmReader/dcm_characteristic_map.py
+++ src/dcmReader/dcm_characteristic_map.py
@@ -1,9 +1,12 @@
 """
 Definition of DCM characteristic map
 """
+from __future__ import annotations
+
+from dcmReader.utils import _DcmBase
 
 
-class DcmCharacteristicMap:
+class DcmCharacteristicMap(_DcmBase):
     """Definition of a characteristic map
 
     Attributes:
@@ -21,68 +24,32 @@ class DcmCharacteristicMap:
                             contains the values from ST/Y.
         x_dimension (int):   Dimension in x direction of the characteristic maps
         y_dimension (int):   Dimension in y direction of the characteristic maps
-        x_mapping (str):    Mapping of the x axis to a distribution, if available as a comment in DCM
-        y_mapping (str):    Mapping of the y axis to a distribution, if available as a comment in DCM
-        comment (str):      Block comment
     """
 
-    def __init__(self, name) -> None:
-        self.name = name
-        self.values = dict()
-        self.description = None
-        self.display_name = None
-        self.variants = dict()
-        self.function = None
-        self.unit_x = None
-        self.unit_y = None
-        self.unit_values = None
-        self.x_dimension = 0
-        self.y_dimension = 0
-        self.x_mapping = None
-        self.y_mapping = None
-        self.comment = None
-        self._type_name = "KENNFELD"
+    def __init__(
+        self,
+        name: str,
+        values: list[list[float]] | None = None,
+        coords: tuple[list[float], ...] = None,
+        attrs: dict | None = None,
+        *,
+        block_type: str = "KENNFELD",
+    ) -> None:
+        if attrs is None:
+            attrs = {
+                "description": "",
+                "display_name": "",
+                "variants": {},
+                "function": "",
+                "units_x": "",
+                "units_y": "",
+                "units": "",
+            }
 
-    def __str__(self):
-        value = f"{self._type_name} {self.name} {self.x_dimension} {self.y_dimension}\n"
-
-        if self.comment:
-            for line in self.comment.splitlines(True):
-                value += f"* {line}"
-        if self.description:
-            value += f'  LANGNAME      "{self.description}"\n'
-        if self.function:
-            value += f'  FUNKTION      "{self.function}"\n'
-        if self.display_name:
-            value += f"  DISPLAYNAME   {self.display_name}\n"
-        if self.unit_x:
-            value += f'  EINHEIT_X     "{self.unit_x}"\n'
-        if self.unit_y:
-            value += f'  EINHEIT_Y     "{self.unit_y}"\n'
-        if self.unit_values:
-            value += f'  EINHEIT_W     "{self.unit_values}"\n'
-        if self.x_mapping:
-            value += f'*SSTX   {self.x_mapping}\n'
-        if self.y_mapping:
-            value += f'*SSTY   {self.y_mapping}\n'
-        stx_written = False
-        for y_entry, map_values in self.values.items():
-            x_entries = ""
-            value_entries = ""
-            for x_entry, value_entry in map_values.items():
-                x_entries += f"{str(x_entry)} "
-                value_entries += f"{str(value_entry)} "
-            if not stx_written:
-                value += f'  ST/X          {x_entries.strip()}\n'
-                stx_written = True
-            value += f'  ST/Y          {str(y_entry).strip()}\n'
-            value += f'  WERT          {value_entries.strip()}\n'
-        for var_name, var_value in self.variants.items():
-            value += f"  VAR           {var_name}={var_value}\n"
-
-        value += "END"
-
-        return value
-
-    def __lt__(self, other):
-        return self.function < other.function and self.description < other.description
+        super().__init__(
+            name=name,
+            values=values,
+            coords=coords,
+            attrs=attrs,
+            block_type=block_type,
+        )
